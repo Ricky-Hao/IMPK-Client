efa3b090420784279587b8588001c8fa07fb4e1a
RickyHao <a471558277@gmail.com>
2018-04-20 11:49:02 +0800

    Add cert support.

---
+++ client/message/cert.py
@@ -0,0 +1,48 @@
+from .base import BaseMessage
+
+
+class CertificateSigningRequestMessage(BaseMessage):
+    def __init__(self, data=None):
+        super().__init__(data)
+
+    def _init_type(self):
+        self.type = 'CertificateSigningRequestMessage'
+
+    def _parse_dict(self, data):
+        super()._parse_dict(data)
+        self.csr = data['csr']
+
+    def to_dict(self):
+        data = super().to_dict()
+        data['csr'] = self.csr
+        return data
+
+
+class CertificateRequestMessage(BaseMessage):
+    def __init__(self, data=None):
+        super().__init__(data)
+
+    def _init_type(self):
+        self.type = 'CertificateRequestMessage'
+
+
+class CertificateInstallMessage(BaseMessage):
+    def __init__(self, data=None):
+        super().__init__(data)
+
+    def _init_type(self):
+        self.type = 'CertificateInstallMessage'
+
+    def _parse_dict(self, data):
+        super()._parse_dict(data)
+        self.cert_user = data.get('cert_user')
+        self.cert = data.get('cert')
+
+    def to_dict(self):
+        data = super().to_dict()
+        data['cert_user'] = self.cert_user
+        data['cert'] = self.cert
+        return data
+
+
+
